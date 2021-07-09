f3d47f0c394053d227b8d6cb47bdb7e43ec365bc
salimgarcia <68305616+salimgarcia@users.noreply.github.com>
2021-07-09 10:34:45 -0700

    Create email_search.ps1

---
+++ email_search.ps1
@@ -0,0 +1,40 @@
+<#
+Searches for a specific email in a list of files and writes out the
+name of the file(s) that contain the specified email.
+Can also delete the specified email from the file(s), or
+replace it with a new email.
+#>
+
+Param(
+  [string]$filepath="C:\Users\Salim.Garcia\OneDrive - CSNW\Documents\emails", 
+  [string]$email
+)
+
+$files = Get-ChildItem $filepath
+
+<#Searches through each txt file in the emails folder for the specified email, then outputs the file(s) that contain the specified email#>
+ForEach ($file in $files.FullName) {
+  Select-String -AllMatches -Path $file $email -List | Format-Table Filename | Write-Output
+}
+
+
+$option = Read-Host -Prompt '[Q] Quit [D] Delete [R] Replace'
+
+<#Quits the script#>
+If ($option -eq 'Q') {
+}
+
+<#Deletes the email from the file(s)#>
+elseif ($option -eq 'D') {
+  ForEach ($file in $files.FullName) {
+    ((Get-Content -Path $file) -replace $email, "") | Set-Content -Path $file
+  }
+}
+
+<#Replaces the email with a different email#>
+elseif ($option -eq 'R') {
+  $newemail = Read-Host -Prompt 'Enter new email'
+  ForEach ($file in $files.FullName) {
+    ((Get-Content -Path $file) -replace $email, $newemail) | Set-Content -Path $file
+  }
+}
